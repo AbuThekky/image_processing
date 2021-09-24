fe33eaba64947e5388e3905422bbf65244086b5a
AbuThekky <82602169+AbuThekky@users.noreply.github.com>
2021-09-24 10:44:31 +0100

    Update JPGtoPNGconverter.py

--- JPGtoPNGconverter.py
+++ JPGtoPNGconverter.py
@@ -1,1 +1,17 @@
-print(user.followers_count)
+import sys
+import os
+from PIL import Image
+
+# GRAB FIRST AND SECOND ARGUMENT
+image_folder = sys.argv[1]
+output_folder = sys.argv[2]
+
+# CHECK IS NEW/ EXISTS, IF NOT CREATED
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
+for filename in os.listdir(image_folder):
+    img = Image.open(f"{image_folder}{filename}")
+    clean_name = os.path.splitext(filename)
+    img.save(f"{output_folder}{filename}.png", "png")
+    print("all done")
